bd275b5dd142b2b10d9301a9622e1f7e255df1c3
marygutierrez1 <marygutierrezc1@gmail.com>
2025-07-20 23:38:28 -0500

    main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 # Variables de entorno
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "maryinnova")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "EAAG...")
-COHERE_API_KEY = os.getenv("COHERE_API_KEY", "your-cohere-key")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "EAARZBLR6Ufh8BPKaVMyRsrvEUFrmmXJrDHZCHc4qujNLORZCqWD2ITMyBJwqcDZAzEyX5pL73r7ZBgRlGf1tf9c7CIcHUCBPmptKaM8GajYzRJUerSBh00nbGsk0NMap2guQj6HSu8lYQEnjdMirC7XoD5JZCZCKpfU7Uns6NQ7sbYd5SiBVUVXffZBm9S0PdZCEjZC58beC9e9l0b1tACU8WVl7D11Nbbj831IZC893879LZBAKoQZDZD")
+COHERE_API_KEY = os.getenv("COHERE_API_KEY", "1uipgy5hyF7DgqndvZqKSv9FcWipVc66lNnLmS0E")
 
 # Cliente Cohere
 co = cohere.Client(COHERE_API_KEY)
